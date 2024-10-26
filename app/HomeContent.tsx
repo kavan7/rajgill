97674635ad55c2dc63ece9434a40687ef0fac0f5
kavan7 <abeyratnekavan@gmail.com>
2024-10-26 14:32:05 -0400

    adawdd

---
+++ app/HomeContent.tsx
@@ -0,0 +1,73 @@
+"use client";
+
+import Hero from "@/components/Hero";
+import {
+  IconBriefcase,
+  IconMan,
+  IconPhone,
+} from "@tabler/icons-react";
+import { FloatingNav } from "@/components/ui/floating-navbar";
+import { MacbookScrollDemo } from "@/components/Contact";
+import { TracingBeam } from "@/components/ui/tracing-beam";
+import { About } from "@/components/About";
+import { Footer } from "@/components/Footer";
+import Touch from "@/components/Touch";
+import { usePathname } from "next/navigation";
+import { useEffect, useState } from "react";
+
+export default function Home() {
+  const pathname = usePathname();
+  const [key, setKey] = useState(0);
+
+  useEffect(() => {
+    if (pathname === "/") {
+      // Update key to trigger component re-render when back on the homepage
+      setKey((prevKey) => prevKey + 1);
+    }
+  }, [pathname]);
+
+  const navItems = [
+    {
+      name: "Services",
+      id: "services",
+      icon: <IconBriefcase className="h-4 w-4 text-neutral-500 dark:text-white" />,
+    },
+    {
+      name: "Our Team",
+      id: "about",
+      icon: <IconMan className="h-4 w-4 text-neutral-500 dark:text-white" />,
+    },
+    {
+      name: "Contact",
+      id: "touch",
+      icon: <IconPhone className="h-4 w-4 text-neutral-500 dark:text-white" />,
+    },
+  ];
+
+  return (
+    <main key={key} className="relative flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
+      <div className="max-w-7xl w-full">
+        <FloatingNav navItems={navItems} />
+        <TracingBeam>
+          <div id="home">
+            <div className="flex flex-col">
+              <Hero />
+            </div>
+          </div>
+          <About />
+          <hr />
+          <div id="">
+            <MacbookScrollDemo />
+          </div>
+          <div id="" />
+          <hr id="" />
+          <Touch />
+          <hr />
+          <div className="relative flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
+            <Footer />
+          </div>
+        </TracingBeam>
+      </div>
+    </main>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,77 +1,11 @@
-"use client"
-import Hero from "@/components/Hero";
-import Image from "next/image";
-import { IconBriefcase, IconHome, IconMan, IconMessage, IconPhone, IconUser } from "@tabler/icons-react";
-import { FloatingNav } from "@/components/ui/floating-navbar";
-import { MacbookScrollDemo } from "@/components/Contact";
-import { TracingBeam } from "@/components/ui/tracing-beam";
-import { About } from "@/components/About";
-import { Footer } from "@/components/Footer";
-import Touch from "@/components/Touch";
-import { useRouter } from "next/router";
-import { useEffect } from "react";
+// page.tsx
 
-const router = useRouter();
+import HomeContent from './HomeContent';
 
-useEffect(() => {
-  // Reload the page when arriving at the homepage
-  router.reload();
-}, [router.asPath]);
-
-export default function Home() {
-  const navItems = [
-    {
-      name: "Services",
-      id: "services",
-      icon: <IconBriefcase className="h-4 w-4 text-neutral-500 dark:text-white" />,
-    },
-    {
-      name: "Our Team",
-      id: "about",
-      icon: <IconMan className="h-4 w-4 text-neutral-500 dark:text-white" />,
-    },
-    {
-      name: "Contact",
-      id: "touch",
-      icon: (
-        <IconPhone className="h-4 w-4 text-neutral-500 dark:text-white" />
-      ),
-    },
-  ];
+export default function HomePage() {
   return (
- <>    <main className="relative  flex justify-center items-center flex-col overflow-hidden  mx-auto sm:px-10 px-5" >
- 
-      <div className="max-w-7xl w-full " >
-     
-          <FloatingNav navItems={navItems} />
-          <TracingBeam>
-          <div id="home">
-            
-          <div 
-      className="flex flex-col   "
-    >
-        <Hero/>
-        </div>
-        </div>
-       <About/>
-       <hr/>
-       <div id="">
-       <MacbookScrollDemo/>
-       </div>
-       <div id=""/>
-       <hr id=""/>
-       <Touch/>
-       <hr/>
-      <div className="relative  flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
-       <Footer/>
-       </div>
-      </TracingBeam>
-       
-      </div>
-     
+    <main>
+      <HomeContent />
     </main>
-      
-      </>
-
   );
 }
