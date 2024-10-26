c5babb2dc9ce5e5b7ded3df08ebef58aa055e5d3
kavan7 <abeyratnekavan@gmail.com>
2024-10-26 14:51:05 -0400

    sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,5 +1,6 @@
+// next-sitemap.config.js
 module.exports = {
-    siteUrl: 'https://rajgilllaw.com',
-    generateRobotsTxt: true, // Generate a robots.txt file
-  }
-  
+  siteUrl: 'https://rajgilllaw.com', // Replace with your website's URL
+  generateRobotsTxt: true, // (Optional) Generates a `robots.txt` file with the sitemap URL
+  sitemapSize: 7000, // (Optional) Split large sitemaps into smaller files (recommended limit is 50,000 URLs per sitemap)
+}
