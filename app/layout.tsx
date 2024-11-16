27b64e78e8ed755ac7ed7e245b5f291ae2f39e0a
kavan7 <abeyratnekavan@gmail.com>
2024-11-16 18:14:25 -0500

    css

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,6 @@
 import type { Metadata } from "next";
-
 import "./globals.css";
 import { Analytics } from "@vercel/analytics/react";
-import { SpeedInsights } from '@vercel/speed-insights/next';
 
 export const metadata: Metadata = {
   title: "Family Lawyer in Surrey & Abbotsford | Raj Gill Law",
@@ -10,13 +8,13 @@ export const metadata: Metadata = {
   openGraph: {
     title: "Raj Gill Law Corporation - Trusted Family Lawyers",
     description: "Raj Gill Law Corporation offers legal services in Surrey & Abbotsford. Schedule a free consultation today.",
-    images: "https://rajgilllaw.com/assets/preview-image.jpg", // Updated image URL
+    images: "https://rajgilllaw.com/assets/preview-image.jpg",
   },
   twitter: {
     card: "summary_large_image",
     title: "Raj Gill Law Corporation - Trusted Family Lawyers",
     description: "Raj Gill Law Corporation offers legal services in Surrey & Abbotsford. Schedule a free consultation today.",
-    images: "https://rajgilllaw.com/assets/preview-image.jpg", // Same image for Twitter
+    images: "https://rajgilllaw.com/assets/preview-image.jpg",
   },
 };
 
@@ -27,11 +25,29 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      
-      <body  style={{ backgroundImage: `url('/background6.webp')`, opacity: 1  } }>
+      <head>
+        <meta name="robots" content="index, follow" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="canonical" href="https://rajgilllaw.com" />
+        <link
+          rel="preload"
+          href="https://fonts.googleapis.com/css2?family=Cormorant+Garamond:wght@400;700&family=Poppins:wght@400;700&family=Rubik:wght@400;700&display=swap"
+          as="style"
+        />
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css2?family=Cormorant+Garamond:wght@400;700&family=Poppins:wght@400;700&family=Rubik:wght@400;700&display=swap"
+        />
+      </head>
+      <body
+        style={{
+          backgroundImage: `url('/background6.webp')`,
+          backgroundColor: "#f5f5f5", // Fallback color
+          opacity: 1,
+        }}
+      >
         {children}
         <Analytics />
-        <SpeedInsights />
       </body>
     </html>
   );
