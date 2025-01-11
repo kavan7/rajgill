4de991cd1f40cb774ba7d7266ad35348b40d88a0
kavan7 <abeyratnekavan@gmail.com>
2025-01-11 15:00:48 -0500

    added client reviews

--- app/client-testimonials/page.tsx
+++ app/client-testimonials/page.tsx
@@ -18,27 +18,62 @@ const testimonials = [
     title: "3 months ago",
   },
   {
-    quote: "Raj gill is a fabulous family law attorney to work with. He explained everything to me about the case (from the procedure to what to expect) right from the start of our very first meeting. He did so in a way that I could understand and was very prompt and clear with communication the entire time. Only downside was that it took  longer thn the given timeframe. Big thanks to shrea grover who was always available to answer all the queries.",
+    quote: "Raj gill is a fabulous family law attorney to work with. He explained everything to me about the case (from the procedure to what to expect) right from the start of our very first meeting. He did so in a way that I could understand and was very prompt and clear with communication the entire time. Only downside was that it took longer than the given timeframe. Big thanks to shrea grover who was always available to answer all the queries.",
     name: "Kaur Inder",
     title: "5 months ago",
   },
   {
     quote: "Raj & his team has been really understanding & especially have a good hand holding on cases. I definitely recommend them for most who find it tough to get into such conversations, they can really be helpful.",
     name: "Pri D",
-    title: "5 months ago",
+    title: "7 months ago",
   },
   {
-    quote: "Best lawyer in the Surrey.Great customer service. Special thanks to Madhur goyal and shrea..Highly recommend.",
+    quote: "Best lawyer in Surrey. Great customer service. Special thanks to Madhur Goyal and Shrea.. Highly recommend.",
     name: "Rajbir Kaur",
     title: "6 months ago",
   },
   {
-    quote: "Had Pretty amazing experience with this firm. Tried many law firms but wasn’t as satisfied with the work so one of my recommended raj gill best experience so far.",
+    quote: "Had pretty amazing experience with this firm. Tried many law firms but wasn’t as satisfied with the work, so one of my recommended Raj Gill best experience so far.",
     name: "Dilpreet Singh",
     title: "7 months ago",
   },
-  
+  {
+    quote: "Great Service! Highly recommend for your family law needs. Helped me out tremendously. The free consultation was way better than the ones that were charged.",
+    name: "Ili",
+    title: "1 hour ago",
+  },
+  {
+    quote: "Madhur Goyal professionally handled my real estate transaction.",
+    name: "Rabina Ghai",
+    title: "2 weeks ago",
+  },
+  {
+    quote: "Great experience working with this team! They were super helpful and made a tough situation a lot easier to deal with. Always there to answer my questions and explain things in a way that made sense. Definitely recommend if you're looking for caring and knowledgeable family lawyers!",
+    name: "Karan",
+    title: "4 weeks ago",
+  },
+  {
+    quote: "I had an excellent experience with Raj Gill Law. Madhur and Shrea are incredibly knowledgeable and supportive throughout. I highly recommend this firm, especially if you have the chance to work with Madhur and Shrea. Thank you Raj Gill Law Team.",
+    name: "Happy Hayer",
+    title: "a month ago",
+  },
+  {
+    quote: "THANK YOU SO MUCH!! I had an excellent experience with Raj Gill Law. Madhur, one of their lawyers, was incredibly knowledgeable and attentive throughout my case.",
+    name: "Kunwar Kaur",
+    title: "3 months ago",
+  },
+  {
+    quote: "I had a wonderful experience working with Raj Gill Law firm during my divorce process. Mr. Raj Gill & Mr. Madhur Goyal were not only professional, knowledgeable, but also compassionate throughout the entire process. Their work ethics and dedication were exceptional.",
+    name: "Roveena Robinson",
+    title: "8 months ago",
+  },
+  {
+    quote: "My name is Sukhjinder Singh and I just want to share my experience with Raj Gill and Law Corporation. All staff members treated me very well, especially Shrea Grover. She gave advice and guided me very well at every step, and I did the same, and I got great results.",
+    name: "Kamaljit Singh",
+    title: "8 months ago",
+  },
 ];
+
 
 const navItems = [
     {
