34ae5de44312330d59a8815f93e59c799e840eaf
kavan7 <abeyratnekavan@gmail.com>
2025-02-09 20:26:15 -0500

    dwasd

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,13 @@
 /** @type {import('next').NextConfig} */
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+});
+
 const nextConfig = {
   images: {
     domains: ['assets.aceternity.com', 'rajgilllaw.com', 'pbs.twimg.com', 'images.unsplash.com'],
   },
+
   webpack(config) {
     config.optimization.splitChunks = {
       chunks: 'all',
@@ -10,6 +15,7 @@ const nextConfig = {
     };
     return config;
   },
+
   async headers() {
     return [
       {
@@ -25,4 +31,4 @@ const nextConfig = {
   },
 };
 
-export default nextConfig;
+module.exports = withBundleAnalyzer(nextConfig);
