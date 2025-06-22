9f1f8e4919aa354143870436a9eef266b97a6e5e
kavan7 <abeyratnekavan@gmail.com>
2025-06-22 13:50:50 -0400

    new

--- app/utils/hf.ts
+++ app/utils/hf.ts
@@ -1,6 +1,6 @@
 import { HfInference } from "@huggingface/inference";
 
-// Properly access environment variable using process.env.VARIABLE_NAME
+
 const HF_TOKEN: string = process.env.HUGGINGFACE as string;
 
 export const inference: HfInference = new HfInference(HF_TOKEN);
