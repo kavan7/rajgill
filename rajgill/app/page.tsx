b123300671f8310936141940b0db762a887426f8
Kavan Abeyratne <abeyratnekavan@gmail.com>
2025-06-29 01:23:09 -0400

    dawsd

--- rajgill/app/page.tsx
+++ rajgill/app/page.tsx
@@ -22,7 +22,7 @@ export default function Page() {
   return (
     <main className="relative  flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full ">
-   Hi
+   
        
         <TracingBeam>
              {isOpen && <ChatBox />}
